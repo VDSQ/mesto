acfa7a2ab7f04c7f425b186592310eb0c8436177
VDSQ <vwdosrqk@gmail.com>
2023-02-11 15:01:57 +0500

    features: add gh-pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,48 +1,50 @@
 export default class Card {
-  constructor(name, link, config, openImageCallback) {
-    this._name = name;
-    this._link = link;
-    this._config = config;
-    this._openImageCallback = openImageCallback;
+	constructor(name, link, config, openImageCallback) {
+		this._name = name;
+		this._link = link;
+		this._config = config;
+		this._openImageCallback = openImageCallback;
+		
+		this._create();
+	}
 
-    this._create();
-  }
+	_delete = (evt) => {
+		this._card.remove();
+	}
 
-  _delete = (evt) => {
-    this._card.remove();
-  }
+	_toggleLike = (evt) => {
+		evt.target.classList.toggle(this._config.activeLikeButtonClass);
+	};
 
-  _toggleLike = (evt) => {
-    evt.target.classList.toggle(this._config.activeLikeButtonClass);
-  };
+	_handleImageClick = (evt) => {
+		this._openImageCallback(this._name, this._link);
+	};
 
-  _handleImageClick = (evt) => {
-    this._openImageCallback(this._name, this._link);
-  };
+	_setEventListeners = () => {
+		this._deleteButton.addEventListener("click", this._delete);
+		this._likeButton.addEventListener("click", this._toggleLike);
+		this._image.addEventListener("click", this._handleImageClick);
+	};
 
-  _setEventListeners = () => {
-    this._deleteButton.addEventListener("click", this._delete);
-    this._likeButton.addEventListener("click", this._toggleLike);
-    this._image.addEventListener("click", this._handleImageClick);
-  };
+	_createCard = () => {
+		return document.querySelector(this._config.templateSelector).content.querySelector(this._config.cardSelector).cloneNode(true);
+	}
 
-  _create = () => {
-    const template = document.querySelector(this._config.templateSelector).content.cloneNode(true);
+	_create = () => {		
+		this._card = this._createCard();
+		this._title = this._card.querySelector(this._config.titleSelector);
+		this._image = this._card.querySelector(this._config.imageSelector);
+		this._deleteButton = this._card.querySelector(this._config.deleteButtonSelector);
+		this._likeButton = this._card.querySelector(this._config.likeButtonSelector);
 
-    this._card = template.querySelector(this._config.cardSelector);
-    this._title = this._card.querySelector(this._config.titleSelector);
-    this._image = this._card.querySelector(this._config.imageSelector);
-    this._deleteButton = this._card.querySelector(this._config.deleteButtonSelector);
-    this._likeButton = this._card.querySelector(this._config.likeButtonSelector);
+		this._title.textContent = this._name;
+		this._image.alt = this._name;
+		this._image.src = this._link;
 
-    this._title.textContent = this._name;
-    this._image.alt = this._name;
-    this._image.src = this._link;
+		this._setEventListeners();
+	};
 
-    this._setEventListeners();
-  };
-
-  get card() {
-    return this._card;
-  }
+	get card() {
+		return this._card;
+	}
 }
